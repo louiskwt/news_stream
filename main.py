7e661b6419dcddffc630195a4272c19aa0184eb3
Louis Tsang <kawingt2@gmail.com>
2024-12-14 23:21:46 +0800

    cleaned up

--- main.py
+++ main.py
@@ -4,22 +4,17 @@ from bs4 import BeautifulSoup
 
 def bbc_scrapper():
     try:
-
         url = "https://www.bbc.com/news"
-
         respose = requests.get(url)
         respose.raise_for_status()
         soup = BeautifulSoup(respose.content, 'html.parser')
-
         headlines = []
         for link in soup.find_all('a', class_='sc-2e6baa30-0 gILusN'):
             text = link.text.strip()
             if text not in ['British Broadcasting Corporation', 'Video', 'Sport', 'Home', 'Audio', 'Weather', 'Newsletters']:
                 href = 'https://www.bbc.com' + link['href']
-                headlines.append({'title': text, 'link': href})
-        
+                headlines.append({'title': text, 'link': href})        
         return headlines
-
     except requests.exceptions.RequestException as e:
         print(f'Error scraping BBC: {e}')
         return []
@@ -30,7 +25,6 @@ def guardian_scrapper():
         respose = requests.get(url)
         respose.raise_for_status()
         soup = BeautifulSoup(respose.content, 'html.parser')
-
         headlines = []
         for link in soup.find_all('a', class_='dcr-ezvrjj'):
             href = 'https://www.theguardian.com' + link['href']
