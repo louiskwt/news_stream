345a5a0549f58cc19bafa9e266fe6210d84153df
Louis Tsang <kawingt2@gmail.com>
2024-12-20 09:07:36 +0800

    used helper function in Scrapper

--- scrapper.py
+++ scrapper.py
@@ -6,27 +6,26 @@ from bs4 import BeautifulSoup
 class Scrapper:
 
     headlines = None
-    EXCLUDED_WORDS = ['British Broadcasting Corporation', 'Video', 'Sport', 'Home', 'Audio', 'Weather', 'Newsletters']
-
-    def __init__(self, news_url, base_url, class_name, name="news"):
+    
+    def __init__(self, news_url, base_url, class_name, tag, name="news"):
         self.news_url = news_url 
         self.base_url = base_url
         self.name = name
         self.class_name = class_name
+        self.tag = tag
         self.date = date.today().isoformat() 
 
 
-    def scrape(self):
+    def scrape(self, helper=lambda link, scapper: None):
         try:
             respose = requests.get(self.news_url)
             respose.raise_for_status()
             soup = BeautifulSoup(respose.content, 'html.parser')
             headlines = []
             for link in soup.find_all('a', class_=self.class_name):
-                text = link.text.strip()
-                if text not in self.EXCLUDED_WORDS:
-                    href = self.base_url + link['href']
-                    headlines.append({'title': text, 'link': href})        
+                headline = helper(link, self)
+                if headline is not None :
+                    headlines.append(headline)        
             self.headlines = headlines 
         except requests.exceptions.RequestException as e:
             print(f'Error scraping {self.name}: {e}')
