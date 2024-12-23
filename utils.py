290b65f436ebde3b0276413c4c6c04cf7d532763
Louis Tsang <kawingt2@gmail.com>
2024-12-23 20:34:39 +0800

    added csv remove function

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import os 
+
 def process_bbc_headlines(link, scrapper):
     EXCLUDED_WORDS = ['British Broadcasting Corporation', 'Video', 'Sport', 'Home', 'Audio', 'Weather', 'Newsletters']
     title = link.text.strip()
@@ -11,3 +13,10 @@ def process_guadian_headlines(link, scrapper):
     title = link['aria-label']
     href = scrapper.base_url + link['href']
     return {'title': title, 'link': href}
+
+def remove_csv_files():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    files = os.listdir(dir_path)
+    for file in files:
+        if file.endswith(".csv"):
+            os.remove(os.path.join(dir_path, file))
