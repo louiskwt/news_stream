ff8cf5ea2dd3310859f4ec17793dfd9425f57895
Louis Tsang <kawingt2@gmail.com>
2024-12-30 13:21:31 +0800

    fixed yp base url and exported the news from yp

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import sys
 from scrapper import Scrapper
-from utils import process_bbc_headlines, process_guadian_headlines, remove_csv_files
+from utils import process_bbc_headlines, process_guadian_headlines, process_yp_headlines, remove_csv_files
 
 def scrap_news():
     bbc_scrapper = Scrapper("https://www.bbc.com/news", "https://www.bbc.com", 'sc-2e6baa30-0 gILusN', "bbc_news")
     guardian_scrapper = Scrapper("https://www.theguardian.com/international", "https://www.theguardian.com", 'dcr-ezvrjj', 'guardian_news')
-    yp_scrapper = Scrapper("https://www.scmp.com/yp/", "https://www.scmp.com/", "FrontCard__Title-sc-1ejow7a-2", "young_post")
+    yp_scrapper = Scrapper("https://www.scmp.com/yp/", "https://www.scmp.com", "FrontCard__Title-sc-1ejow7a-2", name="young_post", parent_tag="h2")
 
     bbc_scrapper.scrape(process_bbc_headlines)
     bbc_scrapper.export_news_csv()
-    yp_scrapper.scrape()
+
+    yp_scrapper.scrape(process_yp_headlines)
+    yp_scrapper.export_news_csv()
 
     guardian_scrapper.scrape(process_guadian_headlines)
     guardian_scrapper.export_news_csv()
