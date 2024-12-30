ac1116a217835dcf89678b13c5ee33d2014753b7
Louis Tsang <kawingt2@gmail.com>
2024-12-30 12:42:38 +0800

    build yp scrapper

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from utils import process_bbc_headlines, process_guadian_headlines, remove_csv_f
 def scrap_news():
     bbc_scrapper = Scrapper("https://www.bbc.com/news", "https://www.bbc.com", 'sc-2e6baa30-0 gILusN', "bbc_news")
     guardian_scrapper = Scrapper("https://www.theguardian.com/international", "https://www.theguardian.com", 'dcr-ezvrjj', 'guardian_news')
+    yp_scrapper = Scrapper("https://www.scmp.com/yp/", "https://www.scmp.com/", "FrontCard__Title-sc-1ejow7a-2", "young_post")
 
     bbc_scrapper.scrape(process_bbc_headlines)
     bbc_scrapper.export_news_csv()
+    yp_scrapper.scrape()
 
     guardian_scrapper.scrape(process_guadian_headlines)
     guardian_scrapper.export_news_csv()
